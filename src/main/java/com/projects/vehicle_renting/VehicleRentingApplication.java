85658b3004cc3dbe166e9bfee6e652efe7308f28
abdulmalik <abdelmaleka011@gmail.com>
2026-02-03 16:40:33 +0200

    Initial Spring Boot setup

---
+++ src/main/java/com/projects/vehicle_renting/VehicleRentingApplication.java
@@ -0,0 +1,13 @@
+package com.projects.vehicle_renting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleRentingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleRentingApplication.class, args);
+    }
+
+}
