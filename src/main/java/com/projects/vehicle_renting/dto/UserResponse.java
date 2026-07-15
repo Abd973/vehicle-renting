81d9e7ca32252238bd2000bbcf18561ed84fcefc
abdulmalik <abdelmaleka011@gmail.com>
2026-07-16 00:29:53 +0300

    feat: add UserResponse DTO

---
+++ src/main/java/com/projects/vehicle_renting/dto/UserResponse.java
@@ -0,0 +1,26 @@
+package com.projects.vehicle_renting.dto;
+
+import com.projects.vehicle_renting.model.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserResponse {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String name;
+    private String phone;
+    private Role role;
+    private BigDecimal walletBalance;
+    private LocalDateTime createdAt;
+}
